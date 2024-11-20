5fa453493e82562b42cbefa785e1f0327c99449d
edifer21 <edifer5432@gmailcom>
2024-11-20 14:27:44 +0100

    empezando

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: patferna <patferna@student.42madrid.com>   #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-11-20 12:49:57 by patferna          #+#    #+#             */
+/*   Updated: 2024-11-20 12:49:57 by patferna         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_KINE_H
+
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdint.h>
+# include "get_next_line.h"
+
+int		ft_printf(char const *word, ...);
+int		ft_putchar(char c);
+int		ft_putstr(char *str);
+int		ft_putptr(void *ptr);
+int		ft_putnbr(int n);
+int		ft_putnbr_u(unsigned int n);
+int		ft_putnbr_hex(unsigned long n);
+int		ft_putnbr_hex_m(unsigned long n);
+
+#endif
