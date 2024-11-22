8325b009ce76e60b4c932b81e959ffba0af2876c
edifer21 <edifer5432@gmailcom>
2024-11-22 15:05:55 +0100

    reapaso

--- get_next_line.c
+++ get_next_line.c
@@ -10,12 +10,37 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+/*
 char	*get_next_line(int fd)
 {
-	
+	char str[BUFFER_SIZE + 1]; 
+	int leer;
+	if  (fd = -1)
+		int fd = open ("texto", O_RDONLY);
+	return (1);
+	leer = read(texto, str,10);
+	close (texto);
 }
+*/
 
+char	*get_next_line(int fd)
+{
+	char	buffer[BUFFER_SIZE + 1];
+	read();
+}
 int	main(void)
 {
-	get_next_line(fd);
+	//get_next_line(fd);
+	// char	*get_next_line(int fd)
+	char str[20]; 
+	ssize_t leer;
+	if  (fd = -1)
+	int fd = open ("texto", O_RDONLY);
+	return (1);
+	leer = read("texto.txt", str,19);
+	str[leer] = '\0';
+	close (texto);
 }
